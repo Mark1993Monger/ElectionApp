5a70f6c22c8c9ffeac45d085d37d7e56582b7b8e
unknown <markmonger1993@gmail.com>
2024-07-15 18:44:13 +0000

    This is a edited version of my Electionapp

--- server.js
+++ server.js
@@ -7,53 +7,53 @@ const body = require('body-parser');
 app.use(express.static(path.join(__dirname, 'views')));
 app.set('views engine', 'ejs');
 
-app.use(body.urlencoded(
-    {
-        expended: 'true'
-    }
-))
-
-app.get('/signup', (req, res) => {
-    res.render('signup.ejs');
-    
-});
-
-app.post('/login', (req, res) =>{
-    
-    const {name, password, email} = req.body;
-
-    const userData = {
-        name,
-        password,
-        email
-    }
-    // res.send("Registration successful");
-    res.redirect('login');
-   setInterval(() => {
-    res.redirect('login.ejs')
-   }, 1000)
-    
-})
-
-const sqlite3 = require('sqlite3').verbose()
-const db = new sqlite3.Database(':memory:')
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./text.db');
 
 db.serialize(() => {
 //   db.run('CREATE TABLE lorem (info TEXT)')
 //   const stmt = db.prepare('INSERT INTO lorem VALUES (?)')
 
-  for (let i = 0; i < 10; i++) {
-    stmt.run(`Ipsum ${i}`)
-  }
+  // for (let i = 0; i < 10; i++) {
+  //   stmt.run(`Ipsum ${i}`)
+  // }
 
 //   stmt.finalize()
 
-  db.each('SELECT ***** FROM auth', (err, row) => {
+  db.each('SELECT * FROM auth', (err, row) => {
     console.log(row)
   })
 })
 
 db.close();
+
+app.use(body.urlencoded(
+  {
+      expended: 'true'
+  }
+))
+
+app.get('/signup', (req, res) => {
+  res.render('signup.ejs');
+  
+});
+
+app.post('/login', (req, res) =>{
+  
+  const {name, password, email} = req.body;
+
+  const userData = {
+      name,
+      password,
+      email
+  }
+  // res.send("Registration successful");
+  res.redirect('login');
+ setInterval(() => {
+  res.redirect('login.ejs')
+ }, 1000)
+  
+})
 
 app.get('/login', (req, res) => {
     res.render('login.ejs')
